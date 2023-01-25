d9be1c6ef0e1dbc7824269ea9084c6989bf12f57
Paul Bowsher <paul.bowsher@gmail.com>
2023-01-25 22:02:09 +0000

    Working DNS module

--- flake.nix
+++ flake.nix
@@ -1,29 +1,34 @@
 {
   description = "DNS server for LANCache";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
   inputs.cache-domains.url = "github:uklans/cache-domains";
   inputs.cache-domains.flake = false;
 
-  outputs = inputs@{ self, nixpkgs, cache-domains }:
+  outputs = inputs@{ self, cache-domains }:
+    with builtins;
     let
       system = "x86_64-linux";
-      inherit (builtins) listToAttrs mapAttrs attrNames attrValues flatten split match readFile readDir filter toFile substring;
-    in {
-      dns = { config, lib, pkgs, ... }:
-      let
-        domains = filter
-          (d: d != "" && match "^#.*" d == null)
-          (lib.lists.flatten (
-            map (f: split "\n" (
-                readFile (cache-domains + "/${f}")
-              )
-            )
-            (filter (f: match ".*txt$" f != null) (attrNames (readDir cache-domains)))
-          ));
+    in
+    {
+      nixosModules = {
+        dns = { config, lib, pkgs, ... }:
+          with lib;
+          let
+            cfg = config.lancache.dns;
 
-        ip = "1.2.3.7";
-        zonefile = toFile "zonefile" "
+            domains = filter
+              (d: d != "" && match "^#.*" d == null)
+              (lib.lists.flatten (
+                map
+                  (f: split "\n" (
+                    readFile (cache-domains + "/${f}")
+                  )
+                  )
+                  (filter (f: match ".*txt$" f != null) (attrNames (readDir cache-domains)))
+              ));
+
+            ip = cfg.cacheIp;
+            zonefile = toFile "zonefile" "
 \$TTL    600
 @       IN  SOA ns1 dns.lancache.net. (
             ${substring 0 8 cache-domains.lastModifiedDate}
@@ -37,17 +42,34 @@ ns1     IN  A   ${ip}
 @       IN  A   ${ip}
 *       IN  A   ${ip}
 ";
-      in {
-        config = {
-          services.bind = {
-            enable = true;
-            forwarders = [ "1.1.1.1" "8.8.8.8" ];
-            zones = listToAttrs (map (d: { name = d; value = { master = true; file = zonefile; }; }) (domains));
-          };
+          in
+          {
+            options = {
+              lancache.dns = {
+                enable = mkEnableOption "Enables the Lancache DNS server";
+                forwarders = mkOption {
+                  description = "Upstream DNS servers. Defaults to CloudFlare and Google public DNS";
+                  type = with types; listOf str;
+                  default = [ "1.1.1.1" "8.8.8.8" ];
+                };
+                cacheIp = mkOption {
+                  description = "IP of cache server to advertise via DNS";
+                  type = with types; str;
+                };
+              };
+            };
 
-          networking.firewall.allowedTCPPorts = [ 53 ];
-          networking.resolvconf.useLocalResolver = false;
-        };
+            config = mkIf cfg.enable {
+              services.bind = {
+                enable = true;
+                forwarders = cfg.forwarders;
+                zones = listToAttrs (map (d: { name = d; value = { master = true; file = zonefile; }; }) (domains));
+              };
+
+              networking.firewall.allowedTCPPorts = [ 53 ];
+              networking.resolvconf.useLocalResolver = false;
+            };
+          };
       };
     };
 }
