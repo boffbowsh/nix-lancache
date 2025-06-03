cf04785a55572542e97a1e9b670759356417609f
Paul Bowsher <paulbowsher@monzo.com>
2025-06-03 12:24:18 +0100

    Cross-platform testing

--- flake.nix
+++ flake.nix
@@ -7,21 +7,33 @@
   inputs.monolithic.url = "github:lancachenet/monolithic";
   inputs.monolithic.flake = false;
 
-  outputs = inputs@{ self, cache-domains, monolithic, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      test = pkgs.callPackage ./test.nix;
-      cache = pkgs.callPackage ./cache.nix { inherit monolithic; };
-      dns = pkgs.callPackage ./dns.nix { inherit cache-domains; };
-    in
-    {
-      nixosModules = {
-        cache = cache;
-        dns = dns;
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
+  inputs.flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
+      flake = let
+        cache = { pkgs, monolithic, ... }:
+          pkgs.callPackage ./cache.nix { inherit monolithic; };
+        dns = { pkgs, cache-domains, ... }:
+          pkgs.callPackage ./dns.nix { inherit cache-domains; };
+      in {
+        nixosModules = {
+          cache = cache;
+          dns = dns;
+        };
       };
-      checks.${system} = {
-        lancache = pkgs.callPackage ./test.nix { inherit cache dns; };
+      perSystem = { pkgs, ... }: {
+        checks = {
+          lancache = pkgs.callPackage ./test.nix {
+            cache =
+              pkgs.callPackage ./cache.nix { monolithic = inputs.monolithic; };
+            dns = pkgs.callPackage ./dns.nix {
+              cache-domains = inputs.cache-domains;
+            };
+          };
+        };
       };
     };
 }
